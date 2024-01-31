b6f407af787b2b5926fe7f906a946ec50c92e5d8
ma <mariamarwo2018@gmail.xn--co-pka>
2024-01-31 13:36:09 +0100

    Si l'utilisateur veut arreter le programme qui tape 'stop' ou sinon si il veut contunuer qui met ce qu'il veut en dehors de 'stop'

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
+
 Nom=int(input("Saisit un nombre : "))
 print("tu as saisit le numero "+str(Nom))
 
 for e in range(1,11):
-    mul=Nom * e
+    mul=Nom*e
     print(mul)
+
+B=input("Si tu veux t'arreter, tape 'stop' ou si tu veux continuer tape ce que tu veux: ")
+if B =='stop':
+    print("Fin")
+else:
+    for e in range(1,11):
+        mul=Nom*e
+        print(mul)
