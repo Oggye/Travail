c3021055c8529cbfdfd7ebb1a51b376010e7628e
Oggye <158271137+Oggye@users.noreply.github.com>
2024-10-04 15:31:57 +0200

    Update hello.js

--- hello.py
+++ hello.py
@@ -1,15 +1,42 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
 
-Nom=int(input("Saisit un nombre : "))
-print("tu as saisit le numero "+str(Nom))
+// You can import supported modules from npm
+import { Card } from 'react-native-paper';
 
-for e in range(1,11):
-    mul=Nom*e
-    print(mul)
 
-B=input("Si tu veux t'arreter, tape 'stop' ou si tu veux continuer tape ce que tu veux: ")
-if B =='stop':
-    print("Fin")
-else:
-    for e in range(1,11):
-        mul=Nom*e
-        print(mul)
+import { Image } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Card>
+        <Text style={styles.paragraph}>
+          Hello word
+        </Text>
+        <Image source={{ uri: 'https://imgs.search.brave.com/tN_VLXBlFtZW0c6s2h9tSd5yywtlHvi0BHB4rAeZSiY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuZ2VuaWFsbHku/Y29tL3MzZnMtcHVi/bGljL0ltYWdlJTIw/bGElMjB2aWxsZSUy/MGF1eCUyMG4lQzMl/QTlvbnNmci5wbmc_/VmVyc2lvbklkPXp2/bHBXcmU0aGdydW9W/RVYwM09sRjdkdTMx/ak5HRXcx' }} style={styles.image}/>
+      </Card>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+    image: {
+    width: 150,  
+    height: 150, 
+    alignSelf: 'center', 
+    marginTop: 10, 
+    marginBottom: 20,
+  },
+});
